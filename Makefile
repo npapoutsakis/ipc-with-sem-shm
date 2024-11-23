20320b8b8bd0b1f79c09d9b9d53e39ec24c6c534
npapoutsakis <nikospapoutsak@gmail.com>
2024-11-23 21:32:32 +0200

    fixed randomLine and started parseConfig

--- Makefile
+++ Makefile
@@ -1,12 +1,10 @@
-CC = gcc
-CFLAGS = -Wall -g
 
-all: parent
+all:
+	gcc -o parent_proc parent_proc.c
 
-parent: parent_proc.c
-	gcc -o parent parent_proc.c
-	./parent
+abort:
+	ps -ef | grep "./parent_proc" | grep -v grep | awk '{print $$2}' | xargs -r kill
+	@echo "All parent_proc processes have been terminated."
 
 clean:
-	rm -f parent
-	clear
+	rm -f parent_proc ; clear
--- parent_proc.c
+++ parent_proc.c
@@ -1,70 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
 
 // TODOs
 // need to write a function to handle the cf file
-// need to write a function get random line from the text file
-// need to write a function to spawn children process
-// need to write a functuion to create shared memory segment
+// need to write a function to spawn child process
+// need to write a function to create shared memory segment
+// need to write a function to create semaphores
+// need to be able to handle active child processes & semaphores
 
 #define MAX_COMMAND_SIZE 15
+
+
+// global time variable
+int global_time = 0;
 
 //parse config file as command for the parent
 typedef struct Command {
     int timestamp;      // Timestamp (21, 22, 23, ...)
-    char cid[3];        // Child Process ID (C1, C2, C3, ...)
-    char status;        // S (Spawn), T (Termination), E (Exit)
+    char cid[5];          // Child Process ID (C1, C2, C3, ...)
+    char status[5];       // S (Spawn), T (Termination), E (Exit)
 } Command;
 
 
-// read config file and
-void getCommands(char *filename) {
-    //open file for reading
-    FILE *file = fopen(filename, "r");
-
-    if (file == NULL) {
-        printf("Error opening file\n");
+// Get a non empty random line from text file
+char *getRandomLine(char *filename){
+    // Open file for reading
+    FILE* file = fopen(filename, "r");
+    if(file == NULL) {
+        printf("Error opening file %s\n", filename);
         exit(EXIT_FAILURE);
     }
 
-    char line[MAX_COMMAND_SIZE];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        printf("%s\n", line);
+    // Randomness
+    srand(time(NULL));
+    int line_num = rand() % 1000;
+
+    // line buffer
+    char *buffer = (char *)malloc(1000 * sizeof(char));
+    if(buffer == NULL) {
+        printf("Memory allocation failed\n");
+        fclose(file);
+        exit(EXIT_FAILURE);
     }
 
+    int i = 0;
+    while(fgets(buffer, 1000, file) != NULL) {
+        if (i == line_num) {
+            break;
+        }
+        i++;
+    }
 
     fclose(file);
-    return;
+    return buffer;
 }
 
 
+// Parse commands from the config file
+Command *parseCommand(char *filename) {
 
-char* getRandomLine(char *filename) {
-    //open file for reading
-    FILE *file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error opening file\n");
+    FILE* file = fopen(filename, "r");
+    if(file == NULL) {
+        printf("Error opening file %s\n", filename);
         exit(EXIT_FAILURE);
     }
 
 
 
+
+    return NULL;
 }
 
 
-// todo's
-int spawn_child(){ return 0;}
-int terminate_child() { return 0;}
 
 
-int main() {
 
-    // Command **commands = malloc(10 * sizeof(Command));
 
-    // getCommands("config_3_100.txt");
 
-    // printf("%s\n", getRandomLine("mobydick.txt"));
 
+
+
+
+
+
+
+int main(int argc, char *argv[]) {
+    Command **commands = (Command *)malloc(2 * sizeof(Command));
     return 0;
 }
