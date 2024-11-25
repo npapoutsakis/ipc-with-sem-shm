cde1b75955f8c47c5dc86b1ccae15ebcc455bc24
npapoutsakis <nikospapoutsak@gmail.com>
2024-11-26 01:26:19 +0200

    working but needs debug, double random msges on fork()

--- ipc.c
+++ ipc.c
@@ -7,14 +7,22 @@
 #include <sys/types.h>
 #include <sys/shm.h>        //shared memory
 #include <sys/sem.h>        //semaphores
+#include <sys/wait.h>
+
 
 // shared memory Size
 #define SHM_SIZE 1024
 #define TRUE 1
 #define FALSE 0
 
+
 // global time variable
 int global_time = 0;
+
+
+// create a buffer for shared memory
+char shared_memory[SHM_SIZE];
+
 
 //parse config file as command for the parent
 typedef struct Command {
@@ -24,11 +32,12 @@ typedef struct Command {
 } Command;
 
 
-// create a structure to hold the shared memory segment
-typedef struct SharedMemory {
-    char text[SHM_SIZE];      // For storing message
-    int child_id;             // Child ID to whom message is sent
-} SharedMemory;
+// Simple structure to keep useful info about a child process upon creation
+typedef struct ChildProcess {
+    pid_t pid;                      // Child Process ID
+    int semaphore_index;            // semaphore assigned to the specific child
+    Command *creation_command;      // creation commands executed from parent
+} ChildProcess;
 
 
 // Get a non empty random line from text file
@@ -61,6 +70,7 @@ char *getRandomLine(char *filename){
     fclose(file);
     return buffer;
 }
+
 
 // Parse commands from the config file
 void parseCommands(char *filename, Command ***commands, int *num_commands) {
@@ -113,6 +123,7 @@ void parseCommands(char *filename, Command ***commands, int *num_commands) {
     return;
 }
 
+
 // Free Command Array
 void freeSpace(Command ***commands, int total_commands) {
     // Free the allocated memory
@@ -123,172 +134,179 @@ void freeSpace(Command ***commands, int total_commands) {
     free(cemp); // Free the array of pointers
 }
 
+
 // Sleep in ms
 void sleep_ms(int ms) {
     usleep(ms * 1000);
 }
 
+
 int create_semaphores(int num_semaphores) {
     // create the semaphore set
     int semaphore_id = semget(IPC_PRIVATE, num_semaphores, IPC_CREAT | 0666);
+    if (semaphore_id == -1) {
+        perror("semget failed");
+        exit(EXIT_FAILURE);
+    }
 
-    // init the semaphore set with 1's
+    // init the semaphore set with 0's - blocked state
     for (int i = 0; i < num_semaphores; i++) {
-        semctl(semaphore_id, i, SETVAL, 1);
+        if (semctl(semaphore_id, i, SETVAL, 0) == -1) {
+            perror("semctl failed");
+            exit(EXIT_FAILURE);
+        }
     }
     return semaphore_id;
 }
 
-// clear used semaphores
-void remove_semaphores(int semaphore_id) {
-    if (semctl(semaphore_id, 0, IPC_RMID) == -1) {
-        printf("Semaphores removal failed\n");
-        exit(EXIT_FAILURE);
-    }
-    return;
-}
-
 void wait_semaphore(int semaphore_id, int index) {
     struct sembuf sb = {index, -1, 0}; // Wait (P operation)
-    semop(semaphore_id, &sb, 1);
-    return;
+    if (semop(semaphore_id, &sb, 1) == -1) {
+        perror("semop wait failed");
+        exit(EXIT_FAILURE);
+    }
 }
+
 
 void signal_semaphore(int semaphore_id, int index) {
     struct sembuf sb = {index, 1, 0}; // Signal (V operation)
-    semop(semaphore_id, &sb, 1);
-    return;
+    if (semop(semaphore_id, &sb, 1) == -1) {
+        perror("semop signal failed");
+        exit(EXIT_FAILURE);
+    }
 }
+
+
+// clear used semaphores
+void remove_semaphores(int semaphore_id) {
+    if(semctl(semaphore_id, 0, IPC_RMID) == -1) {
+        perror("Semaphores removal failed\n");
+        exit(EXIT_FAILURE);
+    }
+}
+void handle_termination(int sig) {
+    printf("Child %d received termination signal. Exiting...\n", getpid());
+    shmdt(shared_memory); // Detach shared memory
+    exit(EXIT_SUCCESS);
+}
+
+
+void child_process(int sems_id, int semaphore_index, char *shared_mem) {
+    signal(SIGTERM, handle_termination); // Set up termination handler
+
+    while (TRUE) {
+        wait_semaphore(sems_id, semaphore_index); // Wait for a message
+
+        printf("Child %d received message: %s\n", getpid(), shared_mem);
+
+    }
+}
+
 
 // ./ipc config_3_100.txt mobydick.txt 3
 int main(int argc, char *argv[]) {
 
     // usage error
-    if(argc != 4){
+    if(argc != 4) {
         printf("Usage: %s <config_file> <text_file> <max_processes>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
+    // Randomness
     srand(time(NULL));
 
     // Get commands from config file
     Command **commands = NULL;
     int num_commands = 0;
-    int running = TRUE;
     int max_children = atoi(argv[3]);
 
     // Parsing commands in an array of (struct) Command
     parseCommands(argv[1], &commands, &num_commands);
 
-    // create semaphores for synchronization
+    // create semaphores
     int sems_id = create_semaphores(max_children);
+    // for(int i = 0; i < max_children; i++) {
+
+    // }
 
     //create shared memory segment
-    int shm_id = shmget(IPC_PRIVATE, sizeof(SharedMemory), IPC_CREAT | 0666);
+    int shm_id = shmget(IPC_PRIVATE, sizeof(shared_memory), IPC_CREAT | 0666);
 
     //attach shared memory to parent process
-    SharedMemory *shared_mem = shmat(shm_id, NULL, 0);
+    char *shared_mem = shmat(shm_id, NULL, 0);
 
     // keep track of active children
-    pid_t child_ids[max_children];
+    ChildProcess children[max_children];
+
+    // this will be the index of children[] && semaphores
     int active_children = 0;
 
-    while(running) {
-        // Printing the 'tick' of simulation
-        printf("Tick: %d\n", global_time);
+    //global to -1 - random thought
+    while (TRUE) {
+        printf("Tick %d:\n", global_time);
 
-        for(int i = 0; i < num_commands; i++) {
-            // Exit status found, termination program
-            if(commands[i]->timestamp == global_time && strcmp(commands[i]->status, "EXIT") == 0) {
-                printf("Global time is %d and command is %s -> Program Terminated!\n", global_time, commands[i]->status);
-                running = FALSE;
+        for (int i = 0; i < num_commands; i++) {
+            if (strcmp(commands[i]->status, "EXIT") == 0 && commands[i]->timestamp == global_time) {
+                for (int j = 0; j < active_children; j++) {
+                    kill(children[j].pid, SIGTERM); // Send termination signal
+                    waitpid(children[j].pid, NULL, 0); // Wait for child to exit
+                }
+
+                // Cleanup
+                shmdt(shared_mem);
+                shmctl(shm_id, IPC_RMID, NULL);
+                remove_semaphores(sems_id);
+                freeSpace(&commands, num_commands);
+                printf("Exiting parent process.\n");
+                exit(EXIT_SUCCESS);
             }
-            else if(commands[i]->timestamp == global_time && strcmp(commands[i]->status, "S") == 0)
-            {
-                // Spawn Process Found
-                // assign a semaphore to the process
-                // assign a shared memory segment to the process
 
-                // child_index = 0; at first
-                int child_index = active_children;
+            if (strcmp(commands[i]->status, "S") == 0 && commands[i]->timestamp == global_time) {
+                if (active_children < max_children) {
+                    int child_index = active_children;
+                    pid_t pid = fork();
 
-                pid_t pid = fork();
-
-                if(pid == 0) {
-                    // child process
-                    // assign a semaphore to that process
-
-                    SharedMemory *shared_mem = shmat(shm_id, NULL, 0);
-                    int messages_received = 0;
-
-                    while(TRUE) {
-                        wait_semaphore(sems_id, child_index);
-                        printf("Child %d received: %s\n", child_index, shared_mem->text);
-                        messages_received++;
+                    if (pid == 0) {
+                        // In child process
+                        child_process(sems_id, child_index, shared_mem);
+                    } else if (pid > 0) {
+                        // In parent process
+                        children[child_index].pid = pid;
+                        children[child_index].semaphore_index = child_index;
+                        children[child_index].creation_command = commands[i];
+                        active_children++;
                     }
-
-                    shmdt(shared_mem);
-                    exit(EXIT_SUCCESS);
                 }
-                else if (pid > 0) {
-                    child_ids[active_children] = pid;
-                    active_children++;
-                    printf("Child %d spawned at time %d\n", child_index, global_time);
-                }
-                else {
-                    printf("Failed to spawn child process\n");
-                    exit(EXIT_FAILURE);
-                }
-                break;
             }
 
-            else if(commands[i]->timestamp == global_time && strcmp(commands[i]->status, "T") == 0)
-            {
-                int child_index = active_children - 1;
-                if(child_ids[child_index] > 0) {
-                        kill(child_ids[child_index], SIGTERM);
-                        printf("Terminated child %d\n", child_index + 1);
-                        child_ids[child_index] = 0;
+            if (strcmp(commands[i]->status, "T") == 0 && commands[i]->timestamp == global_time) {
+                for (int j = 0; j < active_children; j++) {
+                    if (children[j].creation_command == commands[i]) {
+                        kill(children[j].pid, SIGTERM);
+                        waitpid(children[j].pid, NULL, 0);
+                        printf("Terminated child process with PID: %d\n", children[j].pid);
                         active_children--;
+
+                        // Shift remaining children down
+                        for (int k = j; k < active_children; k++) {
+                            children[k] = children[k + 1];
+                        }
+                        break;
+                    }
                 }
-                break;
             }
-
         }
 
-        // random data will be sent on each iteration to the active children
-        // 1. attach to shared memory
-        // 2. write data to shared memory
-        // 3. signal to children that data is available
-        // send_random_data();
-
-        if(active_children > 0) {
+        if (active_children > 0) {
             int random_child = rand() % active_children;
-            char *random_line = getRandomLine("mobydick.txt");
-
-            strcpy(shared_mem->text, random_line);
-            shared_mem->child_id = random_child;
-
-            // signal i-th semaphore
-            signal_semaphore(sems_id, random_child);
-
-
-            printf("Sent data to child %d: %s", random_child, random_line);
-            free(random_line);
+            char *msg = getRandomLine(argv[2]);
+            strcpy(shared_mem, msg);
+            signal_semaphore(sems_id, children[random_child].semaphore_index);
         }
 
-
-        // Each iteration of the loop is a "tick" of the simulation
         global_time++;
-        // sleep_ms(1000);
-        // sleep(1);
     }
 
 
-
-    // Clean Up
-    shmctl(shm_id, IPC_RMID, NULL);
-    remove_semaphores(sems_id);
-    freeSpace(&commands, num_commands);
     return 0;
 }
